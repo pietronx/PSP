b8d7b728140f2dc68a0f9e9a0d73b3a92fc05afd
Pietronx <petermania31@gmail.com>
2024-12-01 16:55:32 +0100

    .

--- src/hilos/src/raton/Raton.java
+++ src/hilos/src/raton/Raton.java
@@ -5,45 +5,38 @@ public class Raton
     private int tiempoAlimentacion;
 
     //  Constructor
-    Raton(String nombre, int tiempoAlimentacion)
+    public Raton(String nombre, int tiempoAlimentacion)
     {
+        super();
         this.nombre=nombre;
         this.tiempoAlimentacion=tiempoAlimentacion;
-    }
-
-    //  Getters
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getTiempoAlimentacion() {
-        return tiempoAlimentacion;
-    }
-
-    // Setters
-    public void setNombre(String nombre)
-    {
-        this.nombre = nombre;
-    }
-    public void setTiempoAlimentacion(int tiempoAlimentacion)
-    {
-        this.tiempoAlimentacion = tiempoAlimentacion;
     }
 
     //  Metodo comer
     public void comer()
     {
-        System.out.println("");
-        System.out.println("Raton "+getNombre()+" ha comenzado a alimentarse");
-        System.out.println("");
-        System.err.println("Raton "+getNombre()+" ha terminado de alimentarse, ha tardado "+getTiempoAlimentacion()+" minutos");
-
+        try {
+            System.out.printf("Raton %s ha comenzado a alimentarse\n",nombre);
+            // Thread = Hilos
+            Thread.sleep(tiempoAlimentacion * 1000); // 1000 = milisegundos
+            System.out.printf("Raton %s ha terminado de alimentarse, ha tardado %d segundos\n",nombre, tiempoAlimentacion);
+        }catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args)
     {
         //  Creamos un objeto raton
         Raton uno = new Raton("Mickey",3);
+        Raton dos = new Raton("Ricky",4);
+        Raton tres = new Raton("Alfred",6);
+        Raton cuatro = new Raton("Carlos",8);
+
         uno.comer();
+        dos.comer();
+        tres.comer();
+        cuatro.comer();
     }
 }
